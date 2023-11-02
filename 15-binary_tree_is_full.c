6a5167623a505e3b005ec888009d8e1dd14b2baa
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-03 01:13:14 +0200

    Some fixations

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,14 +8,14 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    int left_full = binary_tree_is_full(tree->left);
-    int right_full = binary_tree_is_full(tree->right);
+	int left_full = binary_tree_is_full(tree->left);
+	int right_full = binary_tree_is_full(tree->right);
 
-    return left_full && right_full;
+	return (left_full && right_full);
 }
