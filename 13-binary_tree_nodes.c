ea542ba8d7bb06a7473c354202e19c87943443fb
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-03 01:39:28 +0200

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,18 +4,15 @@
  * binary_tree_nodes - counts the number of nodes
  * @tree: root node to count nodes
  *
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
+	if (tree->left != NULL || tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	else
 		return (0);
-
-	size_t left_nodes = binary_tree_nodes(tree->left);
-	size_t right_nodes = binary_tree_nodes(tree->right);
-
-	return (left_nodes + right_nodes + 1);
 }
