a3e8e74d3bba480548aa56bd10606320b55fb1d8
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-03 01:54:00 +0200

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance of a tree
- * @tree: root node to measure balance
+ * binary_tree_balance - measure the balance of tree
+ * @tree: root node to measure
  *
- * Return: balance of the tree
+ * Return: balance of tree
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t l_height, r_height;
+	size_t l_longeur, r_longeur;
 
 	if (tree == NULL)
 		return (0);
 
-	l_height = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	r_height = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	l_longeur = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	r_longeur = tree->right ?  1 + binary_tree_height(tree->right) : 1;
 
-	return (l_height - r_height);
+	return (l_longeur - r_longeur);
 }
